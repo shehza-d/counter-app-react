dbee8a318705d07bc91c91992d42f66a0613cff1
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-19 03:29:01 +0500

    All Done

---
+++ src/components/counter.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Counter = () => {
+  
+	
+  //useState ka variable for counter
+  const [num, setNum] = useState(0);
+
+  //useState ka Variable for theme mode dark ya light
+  const [themeMode, setThemeMode] = useState(true);
+
+  //function to enable dark mode
+  const toggleMode = () => setThemeMode(!themeMode);
+
+  return (
+    <div className={themeMode ? "dark" : "light"}>
+      <button onClick={toggleMode}>Mode</button>
+
+      <br /><br /><br /><br /><br /><br />
+
+
+      <button onClick={() => { setNum(num - 1)}}> Minus </button>
+
+      <span>{num}</span>
+
+      <button onClick={() => { setNum(num + 1)}}> Plus </button>
+
+    </div>
+  );
+};
+
+export default Counter;
